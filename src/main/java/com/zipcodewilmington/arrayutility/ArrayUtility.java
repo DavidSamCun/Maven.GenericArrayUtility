de6b5b4c13d2398f7236893deb9e44fff1129f55
David <davidsamcun@gmail.com>
2022-04-12 00:50:04 -0400

    done

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,18 +1,19 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
-    List<T> newList = new ArrayList<T>();
+    List<T> masterList = new ArrayList<T>();
+    T[] array;
 
     public ArrayUtility(T[] inputArray) {
         for (T a: inputArray) {
-            newList.add(a);
+            masterList.add(a);
         }
+        array = Arrays.copyOf(inputArray, inputArray.length);
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
@@ -21,7 +22,7 @@ public class ArrayUtility<T> {
 
         mergeArray(arrayToMerge);
         
-        for (T a: newList) {
+        for (T a: masterList) {
             if(a.equals(valueToEvaluate))
             count++;
         }
@@ -29,27 +30,59 @@ public class ArrayUtility<T> {
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
+        mergeArray(arrayToMerge);                               //Merge Array
+        List<T> countedObjects = new ArrayList<>();
+        TreeMap<T, Integer> countingObjects = new TreeMap<>();   //Use this to
+        TreeMap<Integer, T> smallestToLargest = new TreeMap<>();
+        Integer amount;
 
-        mergeArray(arrayToMerge);
+        for (int i = 0; i < masterList.size(); i++){
+            if(!countedObjects.contains(masterList.get(i))){
+                countedObjects.add(masterList.get(i));
+                countingObjects.put(masterList.get(i), 1);
+            }
+            if(countedObjects.contains(masterList.get(i))){
+                amount = countingObjects.get(masterList.get(i));
+                amount++;
+                countingObjects.put(masterList.get(i), amount);
+            }
+        }
 
+        for (int i = 0; i<countedObjects.size(); i++){
+            smallestToLargest.put(countingObjects.get(countedObjects.get(i)), countedObjects.get(i));
+        }
+        return smallestToLargest.get(smallestToLargest.lastKey());
 
-
-        return null;
     }
 
     public void mergeArray(T[] arrayToMerge){
         for (T a: arrayToMerge) {
-            newList.add(a);
+            masterList.add(a);
         }
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
         Integer count = 0;
-        for (T a: newList) {
+        for (T a: masterList) {
             if(a.equals(valueToEvaluate)){
                 count++;
             }
         }
+
         return count;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        int occurences = getNumberOfOccurrences(valueToRemove);
+        List<T> output = new ArrayList<>();
+        for (T a: masterList) {
+            if(!a.equals(valueToRemove)){
+                output.add(a);
+            }
+        }
+        masterList.remove(valueToRemove);
+
+        T[] outputArray = Arrays.copyOf(array, array.length-occurences);
+        return output.toArray(outputArray);
     }
 }
